89c651db8dd96160e2bb79aa3793419acccb6644
Saophalkun Ponlu <phalkunz@silverstripe.com>
2010-03-17 22:40:36 +0000

    MINOR: make mock object class in blog menu as test only class

--- tests/BlogMenuTest.php
+++ tests/BlogMenuTest.php
@@ -3,7 +3,7 @@ class BlogMenuTest extends SapphireTest {
 	static $fixture_file = 'blogmenu/tests/BlogMenuTest.yml';
 	
 	function testBuildJoinClause() {
-		$blogMenu = new MockBlogMenu();
+		$blogMenu = new BlogMenuTest_Mock();
 		$blogMenu->write();
 		$joinClause = $blogMenu->buildJoinClause();
 		
@@ -13,70 +13,70 @@ class BlogMenuTest extends SapphireTest {
 	function testBuildWhereClause() {
 		$method = "buildWhereClause";
 		
-		$whereClause = $this->getObjectAndCall('MockBlogMenu', 'MenuOne', $method);
+		$whereClause = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuOne', $method);
 		$this->assertEquals("BlogMenu_MenuEntries.BlogMenuID IN (1)", $whereClause);
 		
-		$whereClause = $this->getObjectAndCall('MockBlogMenu', 'MenuTwo', $method);
+		$whereClause = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwo', $method);
 		$this->assertEquals("BlogMenu_MenuEntries.BlogMenuID IN (4,5,7,8,6,2)", $whereClause);
 		
-		$whereClause = $this->getObjectAndCall('MockBlogMenu', 'MenuThree', $method);
+		$whereClause = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuThree', $method);
 		$this->assertEquals("BlogMenu_MenuEntries.BlogMenuID IN (9,10,3)", $whereClause);
 	}
 	
 	function testGetDescendantIDs() {
 		$method = "getDescendantIDs";
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuOne', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuOne', $method);
 		$this->assertTrue(empty($ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuTwo', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwo', $method);
 		$this->assertTrue($this->arrayEqual(array(4,5,6,7,8), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuThree', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuThree', $method);
 		$this->assertTrue($this->arrayEqual(array(9,10), $ids));
 	}
 	
 	function testGetAllIDs() {
 		$method = "getAllIDs";
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuOne', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuOne', $method);
 		$this->assertTrue($this->arrayEqual(array(1), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuTwo', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwo', $method);
 		$this->assertTrue($this->arrayEqual(array(2,4,5,6,7,8), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuThree', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuThree', $method);
 		$this->assertTrue($this->arrayEqual(array(3,9,10), $ids));
 	}
 	
 	function testBlogHolderIDs() {
 		$method = "BlogHolderIDs";
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuOne', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuOne', $method);
 		$this->assertTrue($this->arrayEqual(array(101), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuTwo', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwo', $method);
 		$this->assertTrue($this->arrayEqual(array(101, 102), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuTwoOne', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwoOne', $method);
 		$this->assertTrue($this->arrayEqual(array(102), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuTwoTwo', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwoTwo', $method);
 		$this->assertTrue($this->arrayEqual(array(), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuTwoTwoOne', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwoTwoOne', $method);
 		$this->assertTrue($this->arrayEqual(array(), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuTwoTwoTwo', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwoTwoTwo', $method);
 		$this->assertTrue($this->arrayEqual(array(101), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuThree', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuThree', $method);
 		$this->assertTrue($this->arrayEqual(array(102), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuThreeOne', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuThreeOne', $method);
 		$this->assertTrue($this->arrayEqual(array(), $ids));
 		
-		$ids = $this->getObjectAndCall('MockBlogMenu', 'MenuThreeTwo', $method);
+		$ids = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuThreeTwo', $method);
 		$this->assertTrue($this->arrayEqual(array(102), $ids));
 	}
 	
@@ -84,15 +84,15 @@ class BlogMenuTest extends SapphireTest {
 		$method = "customEntries";
 		$params = array('BlogEntry');
 		
-		$dos = $this->getObjectAndCall('MockBlogMenu', 'MenuOne', $method, $params);
+		$dos = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuOne', $method, $params);
 		$titles = $dos->map();
 		$this->assertTrue($this->arrayEqual(array("BlogEntry Two"), $titles));
 		
-		$dos = $this->getObjectAndCall('MockBlogMenu', 'MenuTwo', $method, $params);
+		$dos = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuTwo', $method, $params);
 		$titles = $dos->map();
 		$this->assertTrue($this->arrayEqual(array("BlogEntry One", "BlogEntry Three", "BlogEntry Four"), $titles));
 		
-		$dos = $this->getObjectAndCall('MockBlogMenu', 'MenuThree', $method, $params);
+		$dos = $this->getObjectAndCall('BlogMenuTest_Mock', 'MenuThree', $method, $params);
 		$titles = $dos->map();
 		$this->assertTrue($this->arrayEqual(array("BlogEntry Four", "BlogEntry Five"), $titles));
 	}
@@ -126,7 +126,7 @@ class BlogMenuTest extends SapphireTest {
 /**
  * This is a Mock Object for BlogMenu class for testing private and protected methods 
  */ 
-class MockBlogMenu extends BlogMenu {
+class BlogMenuTest_Mock extends BlogMenu implements TestOnly {
 
 	public function buildJoinClause() {
 		return parent::buildJoinClause();
